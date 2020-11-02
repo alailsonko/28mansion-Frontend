4266ec062b6d73a0b443b55bf8da5addbc4cc37b
alailsonko <aalailson3@gmail.com>
2020-11-02 19:48:38 -0300

    refactor: eslint refactor

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,59 +1,60 @@
 module.exports = {
-  "env": {
-      "browser": true,
-      "es6": true
+  env: {
+    browser: true,
+    es6: true,
   },
-  "extends": [
-      "plugin:react/recommended",
-      "eslint:recommended",
-      "plugin:import/errors",
-      "plugin:import/warnings",
-      "plugin:import/typescript",
-      "airbnb",
-      "plugin:jest/recommended"
+  extends: [
+    'plugin:react/recommended',
+    'eslint:recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+    'airbnb',
+    'plugin:jest/recommended',
   ],
-  "parser": "@typescript-eslint/parser",
-  "parserOptions": {
-      "ecmaFeatures": {
-          "jsx": true
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 6,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint',
+  ],
+  rules: {
+    'jsx-a11y/label-has-associated-control': 'off',
+    'no-console': 'off',
+    'no-shadow': 'off',
+    'no-param-reassign': 'off',
+    'no-use-before-define': 'off',
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
       },
-      "ecmaVersion": 6,
-      "sourceType": "module"
-  },
-  "plugins": [
-      "react",
-      "@typescript-eslint"
-  ],
-  "rules": {
-    "no-console": "off",
-    "no-shadow":"off",
-    "no-param-reassign":"off",
-    "no-use-before-define": "off",
-      "import/extensions": [
-          "error",
-          "ignorePackages",
-          {
-            "js": "never",
-            "jsx": "never",
-            "ts": "never",
-            "tsx": "never"
-          }
-       ],
-      "react/jsx-filename-extension": [2, { "extensions": [".js", ".jsx", ".ts", ".tsx"] }]
+    ],
+    'react/jsx-filename-extension': [2, { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
 
   },
-  "settings": {
-      "import/extensions": [".js", ".jsx", ".ts", ".tsx"],
-      "import/parsers": {
-        "@typescript-eslint/parser": [".ts", ".tsx", ".js", ".jsx"]
+  settings: {
+    'import/extensions': ['.js', '.jsx', '.ts', '.tsx'],
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx', '.js', '.jsx'],
+    },
+    'import/resolver': {
+      typescript: {
+        project: './tsconfig.json',
       },
-      "import/resolver": {
-        "typescript": {
-          "project": "./tsconfig.json"
-        },
-        "node": {
-          "extensions": [".js", ".jsx", ".ts", ".tsx"]
-        }
-      }
-    }
-}
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+  },
+};
