61de7cafdf07c006f4f56adedd8bc3a33f05c63d
alailsonko <aalailson3@gmail.com>
2020-11-02 19:52:30 -0300

    feat: impelements home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
+import Header from '../../components/Header';
 
 import { Container } from './styles';
 
 const Home: React.FC = () => {
   console.log('hello');
   return (
-    <Container>
-      <h1>Home</h1>
-    </Container>
+    <>
+      <Header />
+      <Container>
+        <h1>Home</h1>
+      </Container>
+    </>
+
   );
 };
 
