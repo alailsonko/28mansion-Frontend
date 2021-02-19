247d607a9afccff93993a5b9a96cce8fdb9608ec
alailsonko <aalailson3@gmail.com>
2021-02-19 15:17:51 -0300

    feat: implements posts reducer

--- src/store/middlewares/index.ts
+++ src/store/middlewares/index.ts
@@ -2,8 +2,10 @@ import { combineReducers } from 'redux';
 
 import signin from './Auth/SignIn/signin.reducer';
 import signup from './Auth/SignUp/signup.reducer';
+import posts from './Auth/Posts/posts.reducer';
 
 export default combineReducers({
   signin,
   signup,
+  posts,
 });
