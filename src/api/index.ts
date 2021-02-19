8c67810cf0878d7c37f4adf526c40af5cf5d0d94
alailsonko <aalailson3@gmail.com>
2021-02-19 14:58:36 -0300

    feat: implements getToken

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,7 +1,28 @@
 import axios from 'axios';
 
-const api = axios.create({
-  baseURL: 'https://socialmedia-28mansion.herokuapp.com/api/v1/',
-});
+function createApi(token = null) {
+  const api = axios.create({
+    baseURL: 'https://socialmedia-28mansion.herokuapp.com/api/v1/',
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  return api;
+}
 
-export default api;
+export function getToken() {
+  const persistedAuth = localStorage.getItem('persist:auth');
+
+  let token = null;
+
+  if (persistedAuth) {
+    const persist = JSON.parse(persistedAuth);
+    const signin = JSON.parse(persist.signin);
+    if (signin.session.token) {
+      token = signin.session.token;
+    }
+  }
+  return token;
+}
+
+export default createApi;
