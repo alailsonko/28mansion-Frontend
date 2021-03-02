555d1966c92744aabbd3a4364ed31397ca10080e
alailsonko <aalailson3@gmail.com>
2021-03-01 22:53:11 -0300

    feat: post types redux

---
+++ src/store/middlewares/Posts/posts.types.ts
@@ -0,0 +1,6 @@
+export const GET_POSTS_REQUEST = '@posts/GET_POSTS_REQUEST';
+export const GET_POSTS_SUCCESS = '@posts/GET_POSTS_SUCCESS';
+export const GET_POSTS_FAILURE = '@posts/GET_POSTS_FAILURE';
+export const SAVE_POST_REQUEST = '@posts/SAVE_POST_REQUEST';
+export const SAVE_POST_SUCCESS = '@posts/SAVE_POST_SUCCESS';
+export const SAVE_POST_FAILURE = '@posts/SAVE_POST_FAILURE';
