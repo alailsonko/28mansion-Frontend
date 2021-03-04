10e6d2916dcc4706041d378c997ddba19228348b
alailsonko <aalailson3@gmail.com>
2021-03-04 00:34:43 -0300

    feat: add article route

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -7,14 +7,17 @@ import Register from '../pages/Register';
 import PrivateRoute from './PrivateRoute';
 import AuthRoute from './AuthRoute';
 import { Store } from '../store';
+import Article from '../pages/Article';
 
 function Routes() {
   const isLogged = useSelector((state: Store) => state.signin.isLogged);
   const loading = useSelector((state: Store) => state.signin.loading);
+  console.log('loading2', loading);
 
   return (
     <BrowserRouter>
       <PrivateRoute path="/" exact loading={loading} isLogged={isLogged} component={Home} />
+      <PrivateRoute path="/article/:id" exact loading={loading} isLogged={isLogged} component={Article} />
       <AuthRoute path="/login" exact loading={loading} isLogged={isLogged} component={Login} />
       <AuthRoute path="/register" exact loading={loading} isLogged={isLogged} component={Register} />
     </BrowserRouter>
