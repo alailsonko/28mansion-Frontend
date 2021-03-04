40ef89e68976dce5340d17ec1ec9902925ed228c
alailsonko <aalailson3@gmail.com>
2021-03-04 00:33:09 -0300

    feat: get post by id types

--- src/store/middlewares/Posts/posts.types.ts
+++ src/store/middlewares/Posts/posts.types.ts
@@ -1,6 +1,12 @@
 export const GET_POSTS_REQUEST = '@posts/GET_POSTS_REQUEST';
 export const GET_POSTS_SUCCESS = '@posts/GET_POSTS_SUCCESS';
 export const GET_POSTS_FAILURE = '@posts/GET_POSTS_FAILURE';
+export const GET_POST_REQUEST = '@posts/GET_POST_REQUEST';
+export const GET_POST_SUCCESS = '@posts/GET_POST_SUCCESS';
+export const GET_POST_FAILURE = '@posts/GET_POST_FAILURE';
+export const DELETE_POST_REQUEST = '@posts/DELETE_POST_REQUEST';
+export const DELETE_POST_SUCCESS = '@posts/DELETE_POST_SUCCESS';
+export const DELETE_POST_FAILURE = '@posts/DELETE_POST_FAILURE';
 export const SAVE_POST_REQUEST = '@posts/SAVE_POST_REQUEST';
 export const SAVE_POST_SUCCESS = '@posts/SAVE_POST_SUCCESS';
 export const SAVE_POST_FAILURE = '@posts/SAVE_POST_FAILURE';
