bc3a92bba50442839ccbdc9e19048d1553147e99
alailsonko <aalailson3@gmail.com>
2021-03-06 02:21:42 -0300

    refactor: move section posts logic to a component

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,60 +1,24 @@
-import React, { useEffect } from 'react';
-import { connect, useDispatch } from 'react-redux';
+import React from 'react';
 import Header from '../../components/Header';
 import RichTextEditor from '../../components/RichTextEditor';
-import Post from '../../components/Post';
-import { getPosts } from '../../store/middlewares/Posts/posts.get.actions';
-import { Store } from '../../store';
 import {
   Container,
-  SectionPosts,
   MainSection,
 } from './styles';
+import SectionPosts from '../../components/SectionPosts';
 
-interface OwnProps {
-  loading: boolean
-}
+const Home: React.FC = () => (
+  <>
+    <Header />
+    <Container>
+      <MainSection>
+        <h1>Home</h1>
+        <RichTextEditor />
+        <SectionPosts />
+      </MainSection>
+    </Container>
+  </>
 
-interface StateProps {
-  posts: any
-  token: any
-}
+);
 
-type Props = OwnProps & StateProps
-
-const Home: React.FC<Props> = (props: Props) => {
-  const { token, posts } = props;
-  const { loading } = posts;
-  const dispatch = useDispatch();
-  useEffect(() => {
-    dispatch(getPosts(token));
-  }, [dispatch]);
-  // console.log(posts);
-
-  return loading ? (
-    <div>loading</div>
-  ) : (
-    <>
-      <Header />
-      <Container>
-        <MainSection>
-          <h1>Home</h1>
-          <RichTextEditor />
-          <SectionPosts>
-            { posts.data.map((post: any) => (
-              <Post key={post.id} posts={post} />
-            )) }
-          </SectionPosts>
-        </MainSection>
-      </Container>
-    </>
-
-  );
-};
-
-const mapState = (state: Store) => ({
-  posts: state.posts,
-  token: state.signin.session.token,
-});
-
-export default connect(mapState)(Home);
+export default Home;
